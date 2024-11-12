003895206b8e790b0187e40a7c98ab89459cefb3
Bruce Dubbs <bdubbs@linuxfromscratch.org>
2024-11-11 22:45:14 -0600

    New settings -- lots of changes

---
+++ sm3_settings.cpp
@@ -0,0 +1,128 @@
+#include "sysmon3.h"
+#include "sm3_settings.h"
+
+SM_Settings::SM_Settings( const QString srvr ) : server( srvr ) 
+{
+}
+
+QString SM_Settings::value( const QString key ) const
+{
+   QString s = settings.value( server + "-" + key ).toString();
+
+   if ( s.isEmpty() ) 
+      s = defaults.value( key , "" );
+   
+   return s;
+}
+
+void SM_Settings::setValue( const QString key, const QString value )
+{
+   if ( value == defaults.value( key ) )
+      settings.remove( server + "-" + key );
+   else
+      settings.setValue( server + "-" + key, value );
+}
+
+void SM_Settings::setBoolValue( const QString key, const bool value )
+{
+   QString boolValue;
+   if ( value ) boolValue = "true";
+   else         boolValue = "false";
+
+   setValue( key, boolValue );
+}
+
+bool SM_Settings::contains( const QString key ) const
+{
+   return settings.contains( server + "-" + key );
+}
+
+// Note that for our purposes arrays do not use server
+// They are only used to define configured hosts (servers)
+QStringList SM_Settings::readArray( const QString key )
+{
+   QStringList sl;
+
+   int size = settings.beginReadArray( key );
+   
+   if ( size > 0 )
+   {
+      for ( int i = 0; i < size; i++ ) 
+      {
+         settings.setArrayIndex( i );
+         QString s = settings.value( key ).toString();
+         sl.append( s );
+      }
+   }
+   
+   settings.endArray();
+   return sl;
+}
+
+void SM_Settings::writeArray( const QString key, const QStringList values )
+{
+   settings.remove( key );
+   settings.beginWriteArray( key );
+
+   for ( int i = 0; i < values.size(); i++  )
+   {
+      settings.setArrayIndex(i);
+      settings.setValue( key, values.at( i ) );
+   }
+   settings.endArray();
+}
+
+// Groups are used to define the temperature interface and sensors for a server
+// In the following methods 'group', for now, is always 'temperatures'
+void SM_Settings::removeGroup( const QString group )
+{
+   QString g = server + "-" + group;
+   settings.beginGroup( g );
+   settings.remove( "" );
+   settings.endGroup();
+}
+
+// This is only used in one place - temperature config
+void SM_Settings::addGroup( const QString group, const QStringList values )
+{
+   QString g = server + "-" + group;
+   settings.beginGroup( g );
+
+   for ( int i = 0; i < values.size(); i++ )
+   {
+      QString     entry = values.at( i );
+      QStringList items = entry.split( ';' );
+      QString     key   = items.at( 0 );
+      QString     value = items.at( 1 );
+
+      settings.setValue( key, value );
+   }
+
+   settings.endGroup();
+}
+
+QStringList SM_Settings::readGroup( const QString group )
+{
+   QStringList config;
+   QString     g = server + "-" + group;
+
+   settings.beginGroup( g );
+   QStringList keys = settings.childKeys();
+
+   foreach (const QString &key, keys) 
+      config << settings.value( key ).toString();
+
+   settings.endGroup();
+   return config;
+}
+
+QString SM_Settings::getDefault( const QString key )
+{
+   return defaults.value( key , "" );
+}
+
+void SM_Settings::sync( void )
+{
+   settings.sync();
+}
+
---
+++ sm3_settings.h
@@ -0,0 +1,66 @@
+//! \file sm3_settings.h
+#ifndef SM_SETTINGS_H
+#define SM_SETTINGS_H
+
+#include <QSettings>
+#include <QListWidget>
+
+//!  A class to allow encompass QSettings and server name.
+class SM_Settings : public QObject
+{
+  Q_OBJECT
+  
+  public:
+    //*! \brief A suporting class for settings access
+    SM_Settings( const QString srvr );
+   
+    //! \brief A null destructor.
+    ~SM_Settings() {}
+
+    QString     value       ( const QString key ) const;
+    void        setValue    ( const QString key, const QString value );
+    void        setBoolValue( const QString key, const bool value );
+    bool        contains    ( const QString key ) const;
+
+    // Use to get and set "hosts" in setup.cpp and sysmon3
+    // WE do not use 'server' in the key
+    QStringList readArray ( const QString key );
+    void        writeArray( const QString key, const QStringList values );
+
+    // Used in temperatures config
+    void        removeGroup( const QString key );
+    void        addGroup   ( const QString key, const QStringList values );
+    QStringList readGroup  ( const QString group );
+
+    QString     getDefault ( const QString key );
+    void        sync( void );
+
+  private:
+    QString      server;
+    QSettings    settings;
+
+    const QMap< QString, QString> defaults
+    {   // key              default
+       {"fontFamily",    "DejaVu Sans"}, 
+       {"fontSize",      "12"         }, 
+       {"fontBold",      "false"      },
+       
+       {"labelColor",    "#ffffff"    }, // white
+       {"labelBg",       "#999999"    }, // medium gray
+       {"dataColor",     "#000000"    }, // black
+       {"dataBg",        "#efefef"    }, // light gray
+       {"progressColor", "#308cc6"    }, // blue
+       {"progressBg",    "#ffffff"    }, // white
+
+       {"useTime",       "true"       },
+       {"useDate",       "true"       },
+       {"useUptime",     "true"       },
+       {"useCPU",        "true"       },
+       {"useCPUbar",     "true"       },
+       {"useMemory",     "true"       },
+
+       {"timeFormat",    "HH:mm:ss t" },
+       {"dateFormat",    "ddd d MMM"  }
+    };
+};
+#endif
